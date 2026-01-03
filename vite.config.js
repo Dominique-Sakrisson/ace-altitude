3eda1530fa086d277cba57e505a963087be70869
Dominique Sakrisson <technestedd@gmail.com>
2026-01-02 16:52:58 -0800

    domsakrisson-fix/deploy5: Changes for bundled deploy.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // root: '.',              // current directory is the project root
-  base: "ace-altitude",
+  base: "/ace-altitude",
   build: {
     outDir: 'dist',  
     target: "esnext"      // build into ./dist
