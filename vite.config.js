dc39bad04f2c571a9fb38e2e1e7c065962020a72
Dominique Sakrisson <technestedd@gmail.com>
2026-01-02 16:31:14 -0800

    domsakrisson-fix/deploy2: Change base to the deployed host from github.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: '.',              // current directory is the project root
+  // root: '.',              // current directory is the project root
+  base: "ace-altitude",
   build: {
     outDir: 'dist',  
     target: "esnext"      // build into ./dist
