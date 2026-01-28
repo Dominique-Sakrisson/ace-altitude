046722da985015e432f030f124580989882ef27c
Dominique Sakrisson <technestedd@gmail.com>
2026-01-27 19:02:00 -0800

    Update readme and vite config with deploy instructions.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: '.',              // current directory is the project root (local development path)
+  //root is needed for local development via 'npm run dev'
+  //for deployed code, the root must be commented out or removed
+  //and  base: "/ace-altitude",  must be put in its place.
+  // current directory is the project root (local development path)
+  root: '.',
   // root: "/ace-altitude",              // current directory is the project root
   // base: "/ace-altitude",
   build: {
