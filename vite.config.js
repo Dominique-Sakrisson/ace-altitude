ef01c3c6b56579b7814ee7bdbf0df6f99a59b3b0
Dominique Sakrisson <technestedd@gmail.com>
2026-01-27 20:04:54 -0800

    Update vite build config for staging branch.

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   root: '.',              // current directory is the project root (local development path)
   // root: "/ace-altitude",              // current directory is the project root
-  base: "/ace-altitude/",
+  // base: "/ace-altitude/",
   build: {
-    outDir: 'docs',  
+    // outDir: 'docs',  
+    outDir: 'dist',  
     target: "esnext"      // build into ./dist
   },
 })
